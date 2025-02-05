65fc4e23e334bae0d6f3cbb328ece97ab65aafa9
Luong Han <hgia11062001@gmail.com>
2025-02-05 09:31:39 +0700

    FIX/MenuPageCombo

--- daily-food/src/pages/Menu/Menu.js
+++ daily-food/src/pages/Menu/Menu.js
@@ -12,55 +12,35 @@ const Menu = () => {
     const dataMenu = UseFetch(`https://healthy-food.techtheworld.id.vn/catemenu/${slug[slug.length - 1]}`);
     const dataMenuDay = UseFetch(`https://healthy-food.techtheworld.id.vn/daymenu?idCate=${slug[slug.length - 1]}`);
     const dataMenuFood = UseFetch(`https://healthy-food.techtheworld.id.vn/foodmenu?idCate=${slug[slug.length - 1]}`);
-    const [nowDay, setNowDay] = useState({
-        day: "",
-        hour: "",
-    });
-
+    const [nowDay, setNowDay] = useState({ day: "", hour: "" });
     const [tab, setTab] = useState(0);
+    const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    // Mảng ngày trong tuần
-    const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-
-    const checkDay = () => {
-        const now = new Date();
-        const day = now.getDay();
-        setNowDay({ day, hour: now.getHours() });
-    };
-
-    // Cập nhật tab khi ngày thay đổi
     useEffect(() => {
-        checkDay();
+        const now = new Date();
+        setNowDay({ day: now.getDay(), hour: now.getHours() });
     }, []);
 
-    // Chọn tab tương ứng với ngày hiện tại
     useEffect(() => {
         if (dataMenuDay && dataMenuDay.length > 0) {
             const currentDayName = weekDays[nowDay.day];
             const currentDayIndex = dataMenuDay.findIndex((item) => item.daymenu_day.toUpperCase() === currentDayName);
-
-            if (currentDayIndex !== -1) {
-                setTab(currentDayIndex);
-            }
+            if (currentDayIndex !== -1) setTab(currentDayIndex);
         }
     }, [nowDay, dataMenuDay]);
 
-    // Cập nhật danh sách thực phẩm cho menu mỗi khi data thay đổi
     useEffect(() => {
-        dataMenuDay.forEach((day) => {
-            day.listfood = dataMenuFood.filter((food) => food.foodmenu_idDay === day.daymenu_id && food.foodmenu_idCate === day.daymenu_idCate);
-        });
+        if (dataMenuDay.length > 0 && dataMenuFood.length > 0) {
+            dataMenuDay.forEach((day) => {
+                day.listfood = dataMenuFood.filter((food) => food.foodmenu_idDay === day.daymenu_id && food.foodmenu_idCate === day.daymenu_idCate);
+            });
+        }
     }, [dataMenuDay, dataMenuFood]);
 
     const calculateTotalPrice = (listfood) => {
-        if (listfood) {
-            return listfood.slice(0, 3).reduce((total, foodItem) => total + (foodItem.price || 0), 0);
-        } else {
-            return 0;
-        }
+        return listfood ? listfood.slice(0, 3).reduce((total, foodItem) => total + (foodItem.price || 0), 0) : 0;
     };
 
-    // Kiểm tra nếu dữ liệu chưa được load thì hiển thị loading
     if (!dataMenu || !dataMenuDay || !dataMenuFood) {
         return <div>Loading...</div>;
     }
@@ -71,82 +51,60 @@ const Menu = () => {
                 <img src={bannerMenu} alt="" />
                 <div className="menu-textBanner">
                     <h2>WEEKLY MEAL PLAN</h2>
-                    {dataMenu && dataMenu.map((item) => <h3 key={item.catemenu_id}>{item.catemenu_title}</h3>)}
+                    {dataMenu.map((item) => (
+                        <h3 key={item.catemenu_id}>{item.catemenu_title}</h3>
+                    ))}
                 </div>
             </div>
-
             <Container>
                 <ul className="menu-listmenu">
-                    {dataMenuDay &&
-                        dataMenuDay.map((item, index) => (
-                            <li key={item.daymenu_id} className={`${tab === index && "active"}`} onClick={() => setTab(index)}>
-                                <span className="menu-listmenu_day">{item.daymenu_day}</span>
-                            </li>
-                        ))}
+                    {dataMenuDay.map((item, index) => (
+                        <li key={item.daymenu_id} className={`${tab === index ? "active" : ""}`} onClick={() => setTab(index)}>
+                            <span className="menu-listmenu_day">{item.daymenu_day}</span>
+                        </li>
+                    ))}
                 </ul>
-
                 <div className="menu-detailMenu">
-                    {dataMenuDay &&
-                        dataMenuDay.map((item, index) => (
-                            <div key={item.daymenu_id} className={`menu-content ${tab === index && "active"}`}>
-                                {item.listfood &&
-                                    item.listfood
-                                        .filter((food) => food.allday === 1)
-                                        .sort((a, b) => {
-                                            const priority = ["Breakfast", "Lunch", "Dinner"];
-                                            return priority.indexOf(a.foodmenu_time) - priority.indexOf(b.foodmenu_time);
-                                        })
-                                        .map((food) => (
-                                            <CardMenu
-                                                key={food.foodmenu_id}
-                                                image={food.foodmenu_image}
-                                                name={food.foodmenu_name}
-                                                time={food.foodmenu_time}
-                                                des={food.foodmenu_des}
-                                                calories={food.foodmenu_calories}
-                                                protein={food.foodmenu_protein}
-                                                carbohydrates={food.foodmenu_carbohydrates}
-                                            />
-                                        ))}
-
+                    {dataMenuDay.map((item, index) => (
+                        <div key={item.daymenu_id} className={`menu-content ${tab === index ? "active" : ""}`}>
+                            {item.listfood && item.listfood.length > 0 ? (
+                                item.listfood
+                                    .filter((food) => food.allday === 1)
+                                    .sort((a, b) => ["Breakfast", "Lunch", "Dinner"].indexOf(a.foodmenu_time) - ["Breakfast", "Lunch", "Dinner"].indexOf(b.foodmenu_time))
+                                    .map((food) => (
+                                        <CardMenu
+                                            key={food.foodmenu_id}
+                                            image={food.foodmenu_image}
+                                            name={food.foodmenu_name}
+                                            time={food.foodmenu_time}
+                                            des={food.foodmenu_des}
+                                            calories={food.foodmenu_calories}
+                                            protein={food.foodmenu_protein}
+                                            carbohydrates={food.foodmenu_carbohydrates}
+                                        />
+                                    ))
+                            ) : (
+                                <p>Loading...</p>
+                            )}
+                            {item.listfood && item.listfood.length > 0 && (
                                 <div className="menu-button">
-                                    {dataMenuDay && dataMenuDay.length > 0 && (
-                                        <h5>
-                                            Tổng giá:
-                                            <span
-                                                style={{
-                                                    textDecoration: "line-through",
-                                                    color: "gray",
-                                                    marginLeft: "20px",
-                                                    fontSize: "24px",
-                                                }}
-                                            >
-                                                {Math.floor(calculateTotalPrice(item.listfood) / 1000).toLocaleString("vi-VN") + ".000"} ₫
-                                            </span>
-                                            <span style={{ color: "black", marginLeft: "20px" }}>
-                                                {Math.floor((calculateTotalPrice(item.listfood) * 0.9) / 1000).toLocaleString("vi-VN") + ".000"} ₫
-                                            </span>
-                                        </h5>
-                                    )}
-
+                                    <h5>
+                                        Tổng giá:
+                                        <span style={{ textDecoration: "line-through", color: "gray", marginLeft: "20px", fontSize: "24px" }}>
+                                            {Math.floor(calculateTotalPrice(item.listfood) / 1000).toLocaleString("vi-VN") + ".000"} ₫
+                                        </span>
+                                        <span style={{ color: "black", marginLeft: "20px" }}>{Math.floor((calculateTotalPrice(item.listfood) * 0.9) / 1000).toLocaleString("vi-VN") + ".000"} ₫</span>
+                                    </h5>
                                     <button
                                         className="menu-button_button-1"
-                                        onClick={() =>
-                                            addCart(
-                                                {
-                                                    ...item,
-                                                    price: (calculateTotalPrice(item.listfood) * 0.9).toFixed(2),
-                                                },
-                                                slug[slug.length - 1],
-                                                dataMenu[0]?.catemenu_title || ""
-                                            )
-                                        }
+                                        onClick={() => addCart({ ...item, price: (calculateTotalPrice(item.listfood) * 0.9).toFixed(2) }, slug[slug.length - 1], dataMenu[0]?.catemenu_title || "")}
                                     >
                                         Order for {item.daymenu_day}
                                     </button>
                                 </div>
-                            </div>
-                        ))}
+                            )}
+                        </div>
+                    ))}
                 </div>
             </Container>
         </div>
